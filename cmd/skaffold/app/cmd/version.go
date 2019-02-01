e49f77717a5c8f12d2fd0dbe6c774916ce38de94
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2019-02-01 10:41:53 +0900

    Add version check when running version command

--- cmd/skaffold/app/cmd/version.go
+++ cmd/skaffold/app/cmd/version.go
@@ -17,12 +17,17 @@ limitations under the License.
 package cmd
 
 import (
+	"fmt"
 	"io"
+	"runtime"
+	"time"
 
 	"github.com/GoogleContainerTools/skaffold/cmd/skaffold/app/flags"
 	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/version"
+	"github.com/GoogleContainerTools/skaffold/pkg/webhook/constants"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
+	"github.com/tcnksm/go-latest"
 )
 
 var versionFlag = flags.NewTemplateFlag("{{.Version}}\n", version.Info{})
@@ -44,5 +49,42 @@ func RunVersion(out io.Writer, cmd *cobra.Command) error {
 	if err := versionFlag.Template().Execute(out, version.Get()); err != nil {
 		return errors.Wrap(err, "executing template")
 	}
+
+	// Get the latest release
+	verCheckCh := make(chan *latest.CheckResponse)
+	go func() {
+		githubTag := &latest.GithubTag{
+			Owner:      constants.GithubOwner,
+			Repository: constants.GithubRepo,
+		}
+
+		res, err := latest.Check(githubTag, version.Get().Version)
+
+		if err != nil {
+			return
+		}
+
+		verCheckCh <- res
+	}()
+
+	select {
+	case <-time.After(2 * time.Second):
+	case res := <-verCheckCh:
+		if res.Outdated {
+			fmt.Fprintf(out, "The latest version is v%s. ", res.Current)
+			switch runtime.GOOS {
+			case "darwin":
+				fmt.Fprintf(out, "Please run the following command to update.\n\n")
+				fmt.Fprintf(out, "curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/%s/skaffold-linux-amd64 && chmod +x skaffold && sudo mv skaffold /usr/local/bin\n", res.Current)
+			case "windows":
+				fmt.Fprintf(out, "Please download the latest binary from the following url and palce it in $PATH directory.\n\n")
+				fmt.Fprintf(out, "https://storage.googleapis.com/skaffold/releases/%s/skaffold-windows-amd64.exe\n", res.Current)
+			default:
+				fmt.Fprintf(out, "Please run the following command to update.\n\n")
+				fmt.Fprintf(out, "curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/%s/skaffold-linux-amd64 && chmod +x skaffold && sudo mv skaffold /usr/local/bin\n", res.Current)
+			}
+		}
+	}
+
 	return nil
 }
